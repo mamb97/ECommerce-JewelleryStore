64702fcf1b98504bf106305daee73d8717f8b737
pd <91752637+dprudhvi@users.noreply.github.com>
2022-04-11 15:05:22 -0700

    1. Handling edit profile page and updating new details to DB 2. Included cancel option in view/edit account details html page

--- backend/account.py
+++ backend/account.py
@@ -135,48 +135,65 @@ def account():
         # user_record = account_db.find_one({'email': session["username"]})
         return render_template('account.html', record=user_record, username=session["username"])
 
-    # else:
-    #     input_body = request.form
-    #     firstname = input_body.get('firstname', '').strip().lower()
-    #     lastname = input_body.get('lastname', '').strip().lower()
-    #     email = input_body.get('email', '').strip().lower()
-    #     address = input_body.get('address', '').strip().lower()
-    #     city = input_body.get('city', '').strip().lower()
-    #     state = input_body.get('state', '').strip().lower()
-    #     country = input_body.get('country', '').strip().lower()
-    #     # zipcode = input_body.get('zipcode', '').strip
-    #     password = input_body.get('password', '').strip()
-    #     confpassword = input_body.get('confpassword', '').strip()
-    #     sq1 = input_body.get('sq1', '').strip().lower()
-    #     sq2 = input_body.get('sq2', '').strip().lower()
-    #     sq3 = input_body.get('sq3', '').strip().lower()
-    #
-    #     if not (firstname and lastname and email and address and city and state and country and password and
-    #             confpassword and sq1 and sq2 and sq3):
-    #         return "Mandatory fields are missing"
-    #
-    #     # if len(password) < 6:
-    #     # return "Password should be at least 6 characters"
-    #
-    #     # if password != confpassword:
-    #     #   return "Password and Confirm Password doesn't match"
-    #
-    #     # if not (account_db.find_one({'email': email}) is None):
-    #     #   return "You cannot register with the provided email"
-    #
-    #     # if input_body.get(state, '') == 'state':
-    #     #     return "Please select a state from dropdown"
-    #     # Todo: Add zipcode
-    #     # account_db.update_one(
-    #     #     {'firstname': firstname, 'lastname': lastname, 'email': email, 'address': address, 'city': city,
-    #     #      'state': state, 'country': country, 'password': password, 'sq1': sq1, 'sq2': sq2, 'sq3': sq3,
-    #     #      'orders': []})
-    #
-    #     new_user_record = {}
-    #
-    #     if firstname != user_record.firstname:
-    #         new_user_record['firstname'] = firstname
-    #
-    #     account_db.update_one({'email': session["username"]}, {"$set": new_user_record})
-    #
-    #     return render_template('account.html', record=user_record)
+    else:
+        input_body = request.form
+        firstname = input_body.get('firstname', '').strip().lower()
+        lastname = input_body.get('lastname', '').strip().lower()
+        email = input_body.get('email', '').strip().lower()
+        address = input_body.get('address', '').strip().lower()
+        city = input_body.get('city', '').strip().lower()
+        state = input_body.get('state', '').strip().lower()
+        country = input_body.get('country', '').strip().lower()
+        # zipcode = input_body.get('zipcode', '').strip
+        #password = input_body.get('password', '').strip()
+        #confpassword = input_body.get('confpassword', '').strip()
+        sq1 = input_body.get('sq1', '').strip().lower()
+        sq2 = input_body.get('sq2', '').strip().lower()
+        sq3 = input_body.get('sq3', '').strip().lower()
+
+        if not (firstname and lastname and email and address and city and state and country and sq1 and sq2 and sq3):
+            return "Mandatory fields are missing"
+
+        # if input_body.get(state, '') == 'state':
+        #     return "Please select a state from dropdown"
+        # Todo: Add zipcode
+
+        # account_db.update_one(
+        #     {'firstname': firstname, 'lastname': lastname, 'email': email, 'address': address, 'city': city,
+        #      'state': state, 'country': country, 'password': password, 'sq1': sq1, 'sq2': sq2, 'sq3': sq3,
+        #      'orders': []})
+
+        new_user_record = {}
+
+        if firstname != user_record["firstname"]:
+            new_user_record['firstname'] = firstname
+
+        if lastname != user_record["lastname"]:
+            new_user_record['lastname'] = lastname
+
+        if address != user_record["address"]:
+            new_user_record['address'] = address
+
+        if city != user_record["city"]:
+            new_user_record['city'] = city
+
+        if state != user_record["state"]:
+            new_user_record['state'] = state
+
+        if sq1 != user_record["sq1"]:
+            new_user_record['sq1'] = sq1
+
+        if sq2 != user_record["sq2"]:
+            new_user_record['sq2'] = sq2
+
+        if sq3 != user_record["sq3"]:
+            new_user_record['sq3'] = sq3
+
+
+        print (new_user_record)
+
+        account_db.update_one({'email': session["username"]}, {"$set": new_user_record})
+
+        user_record.update(new_user_record)
+
+        return render_template('account.html', record=user_record, username=session["username"])
