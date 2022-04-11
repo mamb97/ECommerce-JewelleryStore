47a1f0d4f0620f8c16b1508f0cd9e94339ef2ab4
pd <91752637+dprudhvi@users.noreply.github.com>
2022-04-11 13:47:00 -0700

    1. Account page to display user details 2. Editable account page html

--- backend/account.py
+++ backend/account.py
@@ -122,3 +122,61 @@ def orders():
                                                            "%Y%m%d%H%M%S%f").strftime("%m/%d/%Y %H:%M:%S")
         order_records[idx] = record
     return render_template("order.html", orders=order_records, username=session["username"])
+
+
+@account_api.route('/account', methods=('GET', 'POST'))
+def account():
+    if "username" not in session:
+        return redirect(url_for('product_api.product_list'))
+
+    user_record = account_db.find_one({'email': session["username"]})
+
+    if request.method == 'GET':
+        # user_record = account_db.find_one({'email': session["username"]})
+        return render_template('account.html', record=user_record)
+
+    # else:
+    #     input_body = request.form
+    #     firstname = input_body.get('firstname', '').strip().lower()
+    #     lastname = input_body.get('lastname', '').strip().lower()
+    #     email = input_body.get('email', '').strip().lower()
+    #     address = input_body.get('address', '').strip().lower()
+    #     city = input_body.get('city', '').strip().lower()
+    #     state = input_body.get('state', '').strip().lower()
+    #     country = input_body.get('country', '').strip().lower()
+    #     # zipcode = input_body.get('zipcode', '').strip
+    #     password = input_body.get('password', '').strip()
+    #     confpassword = input_body.get('confpassword', '').strip()
+    #     sq1 = input_body.get('sq1', '').strip().lower()
+    #     sq2 = input_body.get('sq2', '').strip().lower()
+    #     sq3 = input_body.get('sq3', '').strip().lower()
+    #
+    #     if not (firstname and lastname and email and address and city and state and country and password and
+    #             confpassword and sq1 and sq2 and sq3):
+    #         return "Mandatory fields are missing"
+    #
+    #     # if len(password) < 6:
+    #     # return "Password should be at least 6 characters"
+    #
+    #     # if password != confpassword:
+    #     #   return "Password and Confirm Password doesn't match"
+    #
+    #     # if not (account_db.find_one({'email': email}) is None):
+    #     #   return "You cannot register with the provided email"
+    #
+    #     # if input_body.get(state, '') == 'state':
+    #     #     return "Please select a state from dropdown"
+    #     # Todo: Add zipcode
+    #     # account_db.update_one(
+    #     #     {'firstname': firstname, 'lastname': lastname, 'email': email, 'address': address, 'city': city,
+    #     #      'state': state, 'country': country, 'password': password, 'sq1': sq1, 'sq2': sq2, 'sq3': sq3,
+    #     #      'orders': []})
+    #
+    #     new_user_record = {}
+    #
+    #     if firstname != user_record.firstname:
+    #         new_user_record['firstname'] = firstname
+    #
+    #     account_db.update_one({'email': session["username"]}, {"$set": new_user_record})
+    #
+    #     return render_template('account.html', record=user_record)
